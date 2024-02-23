26699d5179a068050bc82f161857a52d034bd067
MANIVSMCA <manimv1268@gmail.com>
2024-02-23 11:26:31 +0530

    my movie app2

--- src/screens/UserAccountScreen.tsx
+++ src/screens/UserAccountScreen.tsx
@@ -18,7 +18,7 @@ const UserAccountScreen = ({navigation}: any) => {
 
       <View style={styles.profileContainer}>
         <Image
-          source={require('../assets/image/avatar.png')}
+          source={require('../assets/image/vsmani.png')}
           style={styles.avatarImage}
         />
         <Text style={styles.avatarText}>John Doe</Text>
